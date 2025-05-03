e2e66cec225b5e2713acc5aabd0f5407da3bf2b8
mohibansaricode <mmcode46@gmail.com>
2025-05-03 19:22:11 +0500

    16 questions complete

--- main.dart
+++ main.dart
@@ -177,5 +177,158 @@ void main() {
   //     print("");
   //   }
 
+    // Q no 11: Write a program to make such a pattern like a right angle triangle with
+  // a number which will repeat a number in a row.
+  // The pattern like :
+  //  1
+  //  22
+  //  333
+  //  4444
+
+  //  for (var i = 1; i <= 5; i++) {
+  //     for (var j = 1; j <= i; j++) {
+  //       stdout.write("$i");
+  //     }
+  //     print("");
+  //   }
+
+  // Q no 12: Write a program to make such a pattern like a right angle triangle with
+  // the number increased by 1 using loop..
+  // The pattern like :
+  //  1
+  //  2 3
+  //  4 5 6
+  //  7 8 9 10
+
+  // int currentRow = 1;
+  // int printedInRow = 0;
+
+  // for (int num = 1; num <= 10; num++) {
+  //   stdout.write("$num ");
+  //   printedInRow++;
+
+  //   if (printedInRow == currentRow) {
+  //     print(""); // Move to next line
+  //     currentRow++;
+  //     printedInRow = 0;
+  //   }
+  // }
+
+  //   Q no 13: Write a program to make a pyramid pattern with numbers increased by
+  //   1
+  //  2 3
+  //  4 5 6
+  // 7 8 9 10
+
+  // int rows = 4;
+  //   int currentRow = 1;
+  //   int printedInRow = 0;
+  //   int num = 1;
+
+  //   for (; num <= (rows * (rows + 1)) ~/ 2; num++) {
+  //     // Print spaces only at the start of each row
+  //     if (printedInRow == 0) {
+  //       stdout.write(' ' * (rows - currentRow));
+  //     }
+
+  //     stdout.write('$num ');
+  //     printedInRow++;
+
+  //     if (printedInRow == currentRow) {
+  //       print(""); // Move to next line
+  //       currentRow++;
+  //       printedInRow = 0;
+  //     }
+  //   }
+
+  // Q no 14: Write a program to make such a pattern as a pyramid with an asterisk.
+  //  *
+  //  * *
+  //  * * *
+  // * * * *
+
+  //  int rows = 4;
+  //   int currentRow = 1;
+  //   int printedInRow = 0;
+  //   int num = 1;
+
+  //   for (; num <= (rows * (rows + 1)) ~/ 2; num++) {
+  //     // Print spaces only at the start of each row
+  //     if (printedInRow == 0) {
+  //       stdout.write(' ' * (rows - currentRow));
+  //     }
+
+  //     stdout.write('* ');
+  //     printedInRow++;
+
+  //     if (printedInRow == currentRow) {
+  //       print(""); // Move to next line
+  //       currentRow++;
+  //       printedInRow = 0;
+  //     }
+  //   }
+
+  // Q no 15:Write a program that asks the user for their email and password. If the
+  // email and password match a predefined set of credentials, print "User
+  // login successful." Otherwise, keep asking for the email and password
+  // until the correct credentials are provided?
+
+  // String userEmail = "mmcode@gmail.com";
+  // String password = "12345";
+
+  // bool isTrue = true;
+
+  // while (isTrue) {
+  //   print("Enter your Email");
+  //   String inputEmail = stdin.readLineSync()!;
+
+  //   print("Enter your password");
+  //   String inputPassword = stdin.readLineSync()!;
+
+  //   if (userEmail == inputEmail && password == inputPassword) {
+  //     print("Login Succesfull");
+  //     break;
+  //   } else {
+  //     print("Try again");
+  //   }
+  // }
+
+  // Q no 16: Write a program that asks the user for their email and password. You
+  // are given a list of predefined user credentials (email and password
+  // combinations). If the entered email and password match any of the
+  // credentials in the list, print "User login successful." Otherwise, keep
+  // asking for the email and password until the correct credentials are
+  // provided.
+
+  // List<Map<String, String>> userCredentials = [
+  //   {"email": "mmcode@gmail.com", "password": "123"},
+  //   {"email": "mohibali@gmail.com", "password": "12345"},
+  // ];
+
+  // bool isTrue = true;
+
+  // while (isTrue) {
+  //   print("Enter your Email");
+  //   String inputEmail = stdin.readLineSync()!;
+
+  //   print("Enter your password");
+  //   String inputPassword = stdin.readLineSync()!;
+
+
+  //   for (var i = 0; i < userCredentials.length; i++) {
+  //     if (userCredentials[i]["email"] == inputEmail &&
+  //         userCredentials[i]["password"] == inputPassword) {
+  //       print("Login Succesfull");
+  //       isTrue = false;
+  //     }
+  //   }
+
+  //   if (isTrue == true) {
+  //     print("Try Again");
+  //   }
+  // }
+
+
+
 
 }
